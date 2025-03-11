3cbd5592e35c58d831b23ba9b3694b905371cc20
Carlos <carlos.soares@shcompanytechnology.com>
2025-03-11 19:17:41 -0300

    botão salvar

--- src/app/_criar-nota/Criar_nota.jsx
+++ src/app/_criar-nota/Criar_nota.jsx
@@ -2,17 +2,23 @@
 import { useState } from "react";
 import { IoIosStarOutline } from "react-icons/io";
 import { IoMdStar } from "react-icons/io";
+import { IoAddOutline } from "react-icons/io5";
 
 export default function CriarNota() {
 
     const [checked, setChecked] = useState(false);
+    const [titulo, setTitulo] = useState("");
+    const [nota, setNota] = useState("");
 
     return (
         <div className="div-criar-nova">
             <div className="div-titulo">
                 <input
                     className="input-criar-1"
-                    placeholder="Título" />
+                    placeholder="Título"
+                    type="text"
+                    onChange={(e) => setTitulo(e.target.value)}
+                    value={titulo} />
                 <div className="button-star">
                     <label className="star-checkbox">
                         <input
@@ -30,8 +36,25 @@ export default function CriarNota() {
                 </div>
             </div>
             <div className="div-nova-nota">
-                <textarea className="input-criar-2" placeholder="Criar nota..." />
+                <textarea
+                    className="input-criar-2"
+                    placeholder="Criar nota..."
+                    onChange={(e) => setNota(e.target.value)}
+                    value={nota} />
             </div>
+
+            {titulo && nota &&
+                <div className="div-icone-add">
+                    <div> </div>
+                    <button className="btn-base">
+                        <IoAddOutline className="icone-add" />
+                    </button>
+                </div>
+            }
+
+
+
+
         </div>
     )
 }
